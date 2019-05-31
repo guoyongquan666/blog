6150cd5850bcfaf721c6c8644c28cddfccf5bbca
爸爸 <1845727893@qq.com>
2019-05-31 21:03:36 +0800

    提交:15%

---
+++ xuebuhui/application/admin/model/category.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 19/5/31
+ * Time: 下午4:31
+ */
+
+
+namespace app\admin\model;
+
+use think\Model;
+
+class category extends Model
+{
+    //
+}
---
+++ xuebuhui/application/index/controller/Show.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 19/5/31
+ * Time: 下午4:25
+ */
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Show extends Controller
+{
+
+    //发帖交流
+    public function chat()
+    {
+        return $this->chat();
+
+    }
+}
